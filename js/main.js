d7333f6dbe9b6e77762630dad55061ab7c27215d
yukminnie <dadiaomin@126.com>
2018-09-26 21:21:55 +0800

    最终实现

--- js/main.js
+++ js/main.js
@@ -1,16 +1,44 @@
-$(p1).on('click', function() {
-    $(images).css({
-        transform: 'translateX(0)'
+var allButtons = $('#buttons > span')
+
+for (let i = 0; i < allButtons.length; i++) {
+  $(allButtons[i]).on('click', function(x) {
+    console.log('hi')
+    var index = $(x.currentTarget).index()
+    var p = index * -500
+    $('#images').css({
+      transform: 'translate(' + p + 'px)'
     })
+    n = index
+    allButtons.eq(n)
+      .addClass('red')
+      .siblings('.red').removeClass('red')
+  })
+}
+
+
+
+var n = 0;
+var size = allButtons.length
+allButtons.eq(n % size).trigger('click')
+  .addClass('red')
+  .siblings('.red').removeClass('red')
+
+var timerId = setInterval(() => {
+  n += 1
+  allButtons.eq(n % size).trigger('click')
+    .addClass('red')
+    .siblings('.red').removeClass('red')
+}, 1000)
+
+$('.window').on('mouseenter', function() {
+  window.clearInterval(timerId)
 })
 
-$(p2).on('click', function() {
-    $(images).css({
-        transform: 'translateX(-500px)'
-    })
-})
-$(p3).on('click', function() {
-    $(images).css({
-        transform: 'translateX(-1000px)'
-    })
+$('.window').on('mouseleave', function() {
+  timerId = setInterval(() => {
+    n += 1
+    allButtons.eq(n % size).trigger('click')
+      .addClass('red')
+      .siblings('.red').removeClass('red')
+  }, 3000)
 })
