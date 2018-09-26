a6e4528665f72c0c69241b4fcc512fb35b9940c5
yukminnie <dadiaomin@126.com>
2018-09-26 21:12:21 +0800

    初步实现

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(p1).on('click', function() {
+    $(images).css({
+        transform: 'translateX(0)'
+    })
+})
+
+$(p2).on('click', function() {
+    $(images).css({
+        transform: 'translateX(-500px)'
+    })
+})
+$(p3).on('click', function() {
+    $(images).css({
+        transform: 'translateX(-1000px)'
+    })
+})
